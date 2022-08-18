02892a347c9e126b5382e093a9cf8a6be127798c
alejandro <alejomv21@gmail.com>
2022-08-17 23:49:20 -0500

    11 hubspot mongoDB

--- controller/hubspotMongoSearch.controller.js
+++ controller/hubspotMongoSearch.controller.js
@@ -17,7 +17,7 @@ const hubspotMongoSearchGet = async (req= request, res= response)=>{
 
       await conectarDB(); 
       let seacrh = await serachProperties('1655134740000', 0);
-      for(let j = 4500; j < seacrh.total; j = j + 100){
+      for(let j = 5800; j < seacrh.total; j = j + 100){
       let next = j
       console.log(j)
 
